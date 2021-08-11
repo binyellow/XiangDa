66cb38be0c5009b225d74ad7ac646f0305ae2938
binyellow <571704908@qq.com>
2021-08-12 00:48:02 +0800

    feat: login and insert into db, then add remark

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -13,8 +13,8 @@ import "./index.less";
 // #01a8ec
 export default class Index extends Component {
   state = {
-    current: 0,
-  }
+    current: 0
+  };
 
   componentWillMount() {}
 
@@ -31,15 +31,20 @@ export default class Index extends Component {
 
   componentDidHide() {}
 
-  handleClick (value) {
+  handleClick(value) {
     this.setState({
       current: value
-    })
+    });
+  }
+
+  handleAdd() {
+    Taro.login().then(console.log);
+    Taro.getUserInfo().then(console.log);
   }
 
   render() {
     return (
-      <View className='dashboard'>
+      <View className="dashboard">
         <AtTabs
           current={this.state.current}
           scroll
@@ -54,49 +59,54 @@ export default class Index extends Component {
           onClick={this.handleClick.bind(this)}
         >
           <AtTabsPane current={this.state.current} index={0}>
-            <View style='font-size:18px;text-align:center;height:100px;'>
-            <AtCard
-              note='小Tips'
-              extra='额外信息'
-              title='这是个标题'
-              thumb='http://www.logoquan.com/upload/list/20180421/logoquan15259400209.PNG'
-            >
-              这也是内容区 可以随意定义功能
-            </AtCard>
+            <View style="font-size:18px;text-align:center;height:100px;">
+              <AtCard
+                note="小Tips"
+                extra="额外信息"
+                title="这是个标题"
+                thumb="http://www.logoquan.com/upload/list/20180421/logoquan15259400209.PNG"
+              >
+                这也是内容区 可以随意定义功能
+              </AtCard>
             </View>
           </AtTabsPane>
           <AtTabsPane current={this.state.current} index={1}>
-            <View style='font-size:18px;text-align:center;height:100px;'>
+            <View style="font-size:18px;text-align:center;height:100px;">
               标签页二的内容
             </View>
           </AtTabsPane>
           <AtTabsPane current={this.state.current} index={2}>
-            <View style='font-size:18px;text-align:center;height:100px;'>
+            <View style="font-size:18px;text-align:center;height:100px;">
               标签页三的内容
             </View>
           </AtTabsPane>
           <AtTabsPane current={this.state.current} index={3}>
-            <View style='font-size:18px;text-align:center;height:100px;'>
+            <View style="font-size:18px;text-align:center;height:100px;">
               标签页四的内容
             </View>
           </AtTabsPane>
           <AtTabsPane current={this.state.current} index={4}>
-            <View style='font-size:18px;text-align:center;height:100px;'>
+            <View style="font-size:18px;text-align:center;height:100px;">
               标签页五的内容
             </View>
           </AtTabsPane>
           <AtTabsPane current={this.state.current} index={5}>
-            <View style='font-size:18px;text-align:center;height:100px;'>
+            <View style="font-size:18px;text-align:center;height:100px;">
               标签页六的内容
             </View>
           </AtTabsPane>
         </AtTabs>
 
-        <AtFab>
-          <Text className='at-fab__icon at-icon at-icon-menu'>
-            <AtIcon value='add-circle' size={24} color='#ccc' />
-          </Text>
-        </AtFab>
+        <View className="fab">
+          <AtFab size="small">
+            <AtIcon
+              className="at-fab__icon"
+              onClick={this.handleAdd.bind(this)}
+              value="add"
+              color="#ccc"
+            />
+          </AtFab>
+        </View>
       </View>
     );
   }
